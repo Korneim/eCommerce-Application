fc7e2e54e44c0911944b6aa4b9a44dea559d6ed6
Mike Kornishin <mrkruner1@gmail.com>
2025-05-15 10:10:54 +0300

    feat: add style for card

--- eCommerce-app/src/components/book-card/BookCard.tsx
+++ eCommerce-app/src/components/book-card/BookCard.tsx
@@ -21,10 +21,10 @@ export const BookCard: FC<Props> = ({ bookInfo }) => {
                 <Typography.Text style={{ textAlign: 'start' }} italic>
                     {price} ₽
                 </Typography.Text>
-                <Typography.Paragraph style={{ maxWidth: 200 }} strong>
+                <Typography.Paragraph style={{ maxWidth: 270 }} strong>
                     {title}{' '}
                 </Typography.Paragraph>
-                <Typography.Paragraph style={{ maxWidth: 200 }} ellipsis={{ rows: 2 }} type="secondary">
+                <Typography.Paragraph style={{ maxWidth: 270 }} ellipsis={{ rows: 2 }} type="secondary">
                     {description}
                 </Typography.Paragraph>
             </Flex>
