f89a618142db23219f6894c92b146fca31969aa2
Mayya <162753929+abeilleee@users.noreply.github.com>
2025-05-20 15:19:08 +0300

    Fix/adaptive (#24)
    * fix: add media query for header
    
    * style: change paddings for registration page
    
    * style: change styles for footer
    
    * style: add media for slider content
    
    * style: change styles for footer
    
    * style: fix styles for registration and login pages
    
    * feat: disable hovers on mobile for cards
    
    * fix: fix styles for registration

--- eCommerce-app/src/components/footer/Footer.tsx
+++ eCommerce-app/src/components/footer/Footer.tsx
@@ -8,33 +8,36 @@ export const Footer: FC = () => {
         <Flex justify="center" className={scss.footer} align="center">
             <div className={scss.img}></div>
 
-            <Flex gap={67} className={scss.content}>
+            <Flex className={scss.content}>
                 <Flex vertical className={scss.contacts}>
-                    <Typography.Title level={3}>Контакты</Typography.Title>
-                    <Typography.Text className={scss.adreses}>
-                        ул. Петербургская, 19, Казань, Россия + 7 (931) 111-22-33 Пн-Вс 9.00 - 18.00 bg@rs.school
-                    </Typography.Text>
+                    <Typography.Title level={3} className={scss.title}>
+                        Контакты
+                    </Typography.Title>
+                    <Flex className={scss.adreses}>Адрес: Казань, ул. Петербургская, 19</Flex>
+                    <Flex className={scss.adreses}>Телефон: + 7(931)111-22-33</Flex>
+                    <Flex className={scss.adreses}>Часы работы: 9.00 - 18.00</Flex>
+                    <Flex className={scss.adreses}>Email: bg@rs.school</Flex>
                 </Flex>
 
                 <Flex vertical className={scss.about}>
                     <Typography.Title level={3} className={scss.title}>
-                        О нас
+                        Авторы
                     </Typography.Title>
 
                     <Flex className={scss.name}>
-                        <Typography.Link href="https://github.com/abeilleee" target="_blank">
+                        <Typography.Link className={scss.link} href="https://github.com/abeilleee" target="_blank">
                             Mayya
                         </Typography.Link>
                         <GithubOutlined className={scss.icon} />
                     </Flex>
                     <Flex className={scss.name}>
-                        <Typography.Link href="https://github.com/Korneim" target="_blank">
+                        <Typography.Link className={scss.link} href="https://github.com/Korneim" target="_blank">
                             Mike
                         </Typography.Link>
                         <GithubOutlined className={scss.icon} />
                     </Flex>
                     <Flex className={scss.name}>
-                        <Typography.Link href="https://github.com/redinar" target="_blank">
+                        <Typography.Link className={scss.link} href="https://github.com/redinar" target="_blank">
                             Dinar
                         </Typography.Link>
                         <GithubOutlined className={scss.icon} />
