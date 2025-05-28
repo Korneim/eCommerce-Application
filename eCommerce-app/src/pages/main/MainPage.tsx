77f607e5ac769f0cf8e7da650aa4577878e02c53
Mike Kornishin <mrkruner1@gmail.com>
2025-05-28 11:04:33 +0300

    feat: add api for main page

--- eCommerce-app/src/pages/main/MainPage.tsx
+++ eCommerce-app/src/pages/main/MainPage.tsx
@@ -1,17 +1,78 @@
-import type { FC } from 'react';
+import { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import { Slider } from '../../components/slider/Slider.tsx';
 import { BooksList } from '../../components/book-list/BooksList.tsx';
-import { mockData } from './mockData.ts';
 import { NewsList } from '../../components/news-list/NewsList.tsx';
 import { Flex } from 'antd';
 import css from './main-page.module.scss';
+import { getPaginatedProducts } from '../catalog/getAllBooks.ts';
+import { mapCatalogData } from '../catalog/mapCatalogData.ts';
+import { ProductProjection } from '@commercetools/platform-sdk';
+
+const selectedIds = ['3659ba76-2698-4c1a-8a2f-dfcdff9fb07a'];
+const recommendedSelectedIds = ['2cb390e9-14bb-49da-bd43-eb9943b7a103'];
 
 export const MainPage: FC = () => {
+    const [recommendedProducts, setrecommendedProducts] = useState<ProductProjection[]>([]);
+
+    const [products, setProducts] = useState<ProductProjection[]>([]);
+    const currentPage = 1;
+    const pageSize = 4;
+    const selectedSort = '';
+    const searchText = '';
+
+    const loadBest = useCallback(async () => {
+        try {
+            const { products } = await getPaginatedProducts(
+                currentPage,
+                pageSize,
+                selectedIds,
+                selectedSort,
+                searchText
+            );
+            setProducts(products);
+        } catch (error) {
+            console.error('Ошибка загрузки:', error);
+        }
+    }, []);
+
+    console.log(products);
+    useEffect(() => {
+        loadBest();
+    }, [loadBest]);
+
+    const mappedBooks = useMemo(() => {
+        return products ? mapCatalogData(products) : [];
+    }, [products]);
+
+    const loadRecommended = useCallback(async () => {
+        try {
+            const { products } = await getPaginatedProducts(
+                currentPage,
+                pageSize,
+                recommendedSelectedIds,
+                selectedSort,
+                searchText
+            );
+            setrecommendedProducts(products);
+        } catch (error) {
+            console.error('Ошибка загрузки:', error);
+        }
+    }, []);
+
+    console.log(products);
+    useEffect(() => {
+        loadRecommended();
+    }, [loadRecommended]);
+
+    const recommendMappedBook = useMemo(() => {
+        return products ? mapCatalogData(recommendedProducts) : [];
+    }, [products, recommendedProducts]);
+
     return (
         <Flex vertical={true} gap={15} className={css.wrapper}>
             <Slider />
-            <BooksList title="Хиты продаж" books={mockData} />
-            <BooksList title="Рекомендации" books={mockData} />
+            <BooksList title="Хиты продаж" books={mappedBooks} />
+            <BooksList title="Рекомендации" books={recommendMappedBook} />
             <NewsList />
         </Flex>
     );
