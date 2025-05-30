742b21c8684c5f2f7c831dbfec4e4ed8d76f0cc9
Dinar Farhutdinov <denarey77@gmail.com>
2025-05-30 10:55:51 +0300

    feat: add product main characteristics

--- eCommerce-app/src/pages/product/ImagesSlider.tsx
+++ eCommerce-app/src/pages/product/ImagesSlider.tsx
@@ -1,5 +1,5 @@
-import { Carousel, Modal, Image } from 'antd';
-import { FC, useState } from 'react';
+import { Carousel, Modal, Image, Spin } from 'antd';
+import { FC, useState, useEffect } from 'react';
 import styles from './ProductImageSlider.module.css';
 
 interface ProductImageSliderProps {
@@ -9,6 +9,34 @@ interface ProductImageSliderProps {
 export const ProductImageSlider: FC<ProductImageSliderProps> = ({ images }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [currentSlide, setCurrentSlide] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [loadedImages, setLoadedImages] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (images.length > 0) {
+            setLoading(true);
+
+            const loadImages = async (): Promise<void> => {
+                const promises = images.map((img: string) => {
+                    return new Promise<string>((resolve) => {
+                        const imgObj: HTMLImageElement = document.createElement('img');;
+                        imgObj.src = img;
+                        imgObj.addEventListener('load', () => {
+                            resolve(img);
+                        }, { once: true });
+                    })
+                })
+                const loaded = await Promise.all(promises);
+                setLoadedImages(loaded);
+                setLoading(false);
+            }
+            loadImages();
+        }
+    }, [images]);
+
+    if (loading) {
+        return <Spin size="large" className={styles.spinner} />;
+    }
     const showModal = (index: number) : void => {
         setCurrentSlide(index);
         setIsModalOpen(true);
@@ -30,7 +58,7 @@ export const ProductImageSlider: FC<ProductImageSliderProps> = ({ images }) => {
                 arrows
                 infinite={false}
             >
-                {images.map((img, index) => (
+                {loadedImages.map((img, index) => (
                     <div key={index} className={styles.slide}>
                         <Image
                             src={img}
@@ -59,7 +87,7 @@ export const ProductImageSlider: FC<ProductImageSliderProps> = ({ images }) => {
                     infinite={false}
                     className={styles.modalcarousel}
                 >
-                    {images.map((img, index) => (
+                    {loadedImages.map((img, index) => (
                         <div key={index} className={styles.modalslide}>
                             <Image
                                 src={img}
--- eCommerce-app/src/pages/product/ProductPage.tsx
+++ eCommerce-app/src/pages/product/ProductPage.tsx
@@ -3,6 +3,22 @@ import { useParams } from 'react-router-dom';
 import getBook from './getBook';
 import { Product } from '@commercetools/platform-sdk';
 import { ProductImageSlider } from './ImagesSlider';
+import { Flex } from 'antd';
+import css from './productPage.module.scss'
+
+interface ProductAttribute {
+    name: string;
+    value: string | object | number;
+}
+
+interface ObjectAttribute {
+    key: string;
+    label: string;
+}
+
+const isObjectAttrValue = (value: unknown): value is ObjectAttribute => {
+    return typeof value === 'object' && value !== null && 'key' in value && 'label' in value;
+}
 
 export const ProductPage: FC = () => {
     const { productId } = useParams();
@@ -23,10 +39,98 @@ export const ProductPage: FC = () => {
         if (product) setBook(product);
     })();
     }, [loadProduct])
-    console.log(book?.masterData.current.masterVariant.images?.map(img => img.url) ?? []);
+    const imagesArray = book?.masterData.current.masterVariant.images?.map(img => img.url);
+    const bookMaster = book?.masterData.current.masterVariant;
+    const bookAuthorAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'author');
+    const bookAuthor = bookAuthorAttr
+        ? (typeof bookAuthorAttr.value === 'string'
+            ? bookAuthorAttr.value
+            : '-')
+        : '-';
+
+    const bookPublishingAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'publishing');
+    const bookPublishing = bookPublishingAttr
+        ? (typeof bookPublishingAttr.value === 'string'
+            ? bookPublishingAttr.value
+            : '-')
+        : '-';
+
+    const bookSeriesAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'series');
+    const bookSeries = bookSeriesAttr
+        ? (typeof bookSeriesAttr.value === 'string'
+            ? bookSeriesAttr.value
+            : '-')
+        : '-';
+
+    const bookYearAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'year');
+    const bookYear = bookYearAttr
+        ? (typeof bookYearAttr.value === 'number'
+            ? bookYearAttr.value
+            : '-')
+        : '-';
+
+    let bookCover = ''
+    const bookCoverAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'cover-type');
+    if (bookCoverAttr?.value && isObjectAttrValue(bookCoverAttr.value)) {
+        const bookCoverValue = bookCoverAttr?.value;
+        bookCover = bookCoverValue.label;
+    }
+
+    const bookArticulAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'articul');
+    const bookArticul = bookArticulAttr
+        ? (typeof bookArticulAttr.value === 'string'
+            ? bookArticulAttr.value
+            : '-')
+        : '-';
+
+    let bookLang = ''
+    const bookLangAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'language');
+    if (bookLangAttr?.value && isObjectAttrValue(bookLangAttr.value)) {
+        const bookLangValue = bookLangAttr?.value;
+        bookLang = bookLangValue.label
+    }
+
+    const bookPagesAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'pages');
+    const bookPages = bookArticulAttr
+        ? (typeof bookArticulAttr.value === 'number'
+            ? bookArticulAttr.value
+            : '-')
+        : '-';
+
+    const bookGenresAttr = bookMaster?.attributes?.find((attr: ProductAttribute) => attr.name === 'genre');
+    const bookGenre = bookGenresAttr
+        ? (typeof bookGenresAttr.value === 'string'
+            ? bookGenresAttr.value
+            : '-')
+        : '-';
+    console.log(book);
     return (
         <>
-            {<ProductImageSlider images={book?.masterData.current.masterVariant.images?.map(img => img.url) ?? []} />}
+            <Flex justify='center' className={`${css['product-conatiner']}`}>
+                {<ProductImageSlider images={imagesArray ?? []} />}
+                <Flex className={`${css['description-container']}`}>
+                    <h1>{book?.masterData.current.name['ru']}</h1>
+                    <Flex className={`${css['characteristics-container']}`}>
+                        <h2>О товаре:</h2>
+                        <Flex className={`${css['characteristics']}`}>
+                            <Flex className={`${css['characteristics-name']}`}>
+                                <span>Автор:</span>
+                                <span>Издательство:</span>
+                                <span>Серия:</span>
+                                <span>Год выпуска:</span>
+                                <span>Тип обложки:</span>
+                            </Flex>
+                            <Flex className={`${css['characteristics-values']}`}>
+                                <span>{bookAuthor}</span>
+                                <span>{bookPublishing}</span>
+                                <span>{bookSeries}</span>
+                                <span>{bookYear}</span>
+                                <span>{bookCover}</span>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                </Flex>
+            </Flex>
         </>
     )
 };
