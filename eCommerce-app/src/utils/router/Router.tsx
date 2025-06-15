e4cdb2e4128bc1dcd559ba4bbd1b9f6e0b8352f2
DeFarh <163840209+DeFarh@users.noreply.github.com>
2025-06-15 19:24:25 +0300

    feat: add about us page

--- eCommerce-app/src/utils/router/Router.tsx
+++ eCommerce-app/src/utils/router/Router.tsx
@@ -10,6 +10,7 @@ import useAuthStore from '../../store/useAuthStore';
 import { CatalogPage } from '../../pages/catalog/Catalog.tsx';
 import UserProfilePage from '../../pages/user-profile/UserProfile.tsx';
 import { ProductPage } from '../../pages/product/ProductPage.tsx';
+import AboutUsPage from '../../pages/about-us/aboutUs.tsx';
 
 export const Router: FC = () => {
     const { isLoggedIn } = useAuthStore();
@@ -49,7 +50,7 @@ export const Router: FC = () => {
                 },
                 {
                     path: routes.about,
-                    element: <div>about</div>,
+                    element: <AboutUsPage />,
                 },
                 {
                     path: routes.profile,
